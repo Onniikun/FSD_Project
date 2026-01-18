157015619e228ab9bc530fd0012aa1ef33f5f1af
Onniikun <dominick2004natoza@gmail.com>
2026-01-18 04:51:23 -0600

    Create a new branch, add song links and folder for future components.

---
+++ FSD_Project/src/components/common/songLinks.tsx
@@ -0,0 +1,18 @@
+import "./songLinks.css"
+
+function Links () {
+    return(
+    <>
+    <div className="Links">
+        <ul className="spotify">
+            <a href="https://open.spotify.com/album/1GjWfyDsXFAQTRq8FLZJN2?si=1RQtNRHiSa-WCTgr556O2g"target="_blank">Spotify</a>
+        </ul>
+        <ul className="apple">
+            <a href="https://music.apple.com/us/song/forever/1767151840"target="_blank">Apple Music</a>
+        </ul>
+    </div>
+    </>
+    )
+}
+
+export default Links
--- FSD_Project/src/components/songList.tsx
+++ FSD_Project/src/components/songList.tsx
@@ -1,18 +1,22 @@
-import "./songList.css";
 import songPFP from "../assets/songpictures/SongPFP.jpg"
 import "./songList.css"
+import Links from "./common/songLinks"
+
 
 function SongList() {
     return(
         <>
         <main>
             <img className="songPFP"src={songPFP} alt="songpic" />
-            <div>
-            <h2 className="title">Forever &</h2>
-            <p className="artist">Ejean</p>
-            <p className="date">Release date: 2024</p>
+            <div className="text">
+                <h2 className="title">Forever &</h2>
+                <p className="artist">Ejean</p>
+                <p className="date">Release date: 2024</p>
             </div>
         </main>
+        <div>
+        <Links/>
+        </div>
         </>
     )
 }
