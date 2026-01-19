9157f26f8dcd397d3e54937ca8438a138d0fbb89
Edraque Maglaque <emaglaque2@academic.rrc.ca>
2026-01-18 22:39:45 -0600

    Added some fake data within the Review function, in-line at the moment.

--- FSD_Project/src/components/reviews/Review.tsx
+++ FSD_Project/src/components/reviews/Review.tsx
@@ -8,3 +8,37 @@ type ReviewItem = {
     likes: number;
 };
 
+export default function Review() {
+    const reviews: ReviewItem[] = [
+        {
+            id: 1,
+            user: "MusicLover67",
+            song: "Life of Ophelia",
+            date: "January 18, 2025",
+            rating: 5,
+            comment: "Taytay is the goat omg! I wish i was Ophelia!",
+            likes: 67,
+        },
+        {
+            id: 2,
+            user: "BrunoMarsVancouverAndRaye",
+            song: "I Just Might",
+            date: "October 14, 2026",
+            rating: 5,
+            comment: "Goated song, got it onna loop rn! Can't wait to hear them live in Vancouver this year!",
+            likes: 76,
+        },
+        {
+            id: 3,
+            user: "BINI",
+            song: "Pantropiko",
+            date: "March 22, 2024",
+            rating: 4,
+            comment: "Such a slay tune! BINI NEVER disappoints. ;P",
+            likes: 96,
+        },
+    ];
+
+    
+
+}
