68b414271f85bef8ad7d3e84ac42d6b8b72c6a8d
Onniikun <dominick2004natoza@gmail.com>
2026-01-18 20:39:05 -0600

    Add data iteration to song information, added temporary styling to song links.

--- FSD_Project/src/components/common/songLinks.tsx
+++ FSD_Project/src/components/common/songLinks.tsx
@@ -4,11 +4,13 @@ function Links () {
     return(
     <>
     <div className="Links">
-        <ul className="spotify">
-            <a href="https://open.spotify.com/album/1GjWfyDsXFAQTRq8FLZJN2?si=1RQtNRHiSa-WCTgr556O2g"target="_blank">Spotify</a>
-        </ul>
-        <ul className="apple">
-            <a href="https://music.apple.com/us/song/forever/1767151840"target="_blank">Apple Music</a>
+        <ul>
+            <li className="spotify">
+                <a href="https://open.spotify.com/album/1GjWfyDsXFAQTRq8FLZJN2?si=1RQtNRHiSa-WCTgr556O2g"target="_blank">Spotify</a>
+            </li>
+            <li className="apple">
+                <a href="https://music.apple.com/us/song/forever/1767151840"target="_blank">Apple Music</a>
+            </li>
         </ul>
     </div>
     </>
--- FSD_Project/src/components/songList.tsx
+++ FSD_Project/src/components/songList.tsx
@@ -1,17 +1,27 @@
 import songPFP from "../assets/songpictures/SongPFP.jpg"
 import "./songList.css"
 import Links from "./common/songLinks"
+import type { Song } from "./common/songModel.ts"
 
 
 function SongList() {
+    const songs: Song[] = [
+    {
+        title: "Forever &",
+        artist: "Ejean",
+        genre: "R&B",
+        release_date: 2024,
+    }
+]
     return(
         <>
         <main>
             <img className="songPFP"src={songPFP} alt="songpic" />
-            <div className="text">
-                <h2 className="title">Forever &</h2>
-                <p className="artist">Ejean</p>
-                <p className="date">Release date: 2024</p>
+            <div className="song-info">
+                <h2>{songs[0].title}</h2>
+                <p>{songs[0].artist}</p>
+                <p>Genre: {songs[0].genre}</p>
+                <p>Release date: {songs[0].release_date}</p>
             </div>
         </main>
         <div>
