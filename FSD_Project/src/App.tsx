cafe2d50b0f595440609bbcdc563a65c6acd7c72
Phoenixx Ordonez <pordonez@academic.rrc.ca>
2026-01-18 19:44:01 -0600

    Created a navigation bar component

--- FSD_Project/src/App.tsx
+++ FSD_Project/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Nav from './components/nav/Nav'
 import SongList  from "./components/songList"
+
 
 function App() {
 
   return (
     <>
+      <Nav />
       <SongList />
     </>
   )
---
+++ FSD_Project/src/components/nav/Nav.tsx
@@ -0,0 +1,31 @@
+function Nav() {
+    return(<nav>
+        <div className="page-links">
+            <span>
+                <a href="#">HOME</a>
+            </span>            
+            <span>
+                <a href="#">SONGS</a>
+            </span>
+            <span>
+                <a href="#">LISTS</a>
+            </span>
+        </div>
+        <section className="search">
+            <form action="#">
+                <input type="text" name="search-bar" />
+                <input type="submit" value="Search" className="action"/>
+            </form>
+        </section>
+        <div className="user-management-links">
+            <span>
+                <a href="#">SIGN IN</a>
+            </span>
+            <span>
+                <a href="#">CREATE ACCOUNT</a>
+            </span>
+        </div>
+    </nav>);
+}
+
+export default Nav;
