bb9f0bb00e38b1586e506046d754f25388215f0a
Onniikun <dominick2004natoza@gmail.com>
2026-01-29 14:24:30 -0600

    Removed repeated components, added link model interface, fixed CSS issues, removed links temporarily.

--- src/components/common/song-items/SongItem.tsx
+++ src/components/common/song-items/SongItem.tsx
@@ -1,38 +1,64 @@
 import songPFP from "../../../assets/songpictures/SongPFP.jpg"
+import songPFP2 from "../../../assets/songpictures/SongPFP2.png"
 import "./SongItem.css"
-import Links from "../song-links/SongLinks.tsx"
 import type { Song } from "../../../types/songModel.ts"
 
 
+
 function SongItem() {
+    // For now it the song list will remain here until we have a
+    // larger database.
     const songs: Song[] = [
     {
+        id: 1,
         title: "Forever &",
         artist: "Ejean",
         genre: "R&B",
         release_date: new Date("July, 12, 2024"),
-        runtime: "2:27"
+        runtime: "2:27",
+        cover: songPFP,
+        links: {
+            spotify: "https://open.spotify.com/album/1GjWfyDsXFAQTRq8FLZJN2?si=8WCiBr8MT1i0r-X6eKvqZw",
+            apple: "https://music.apple.com/us/song/forever/1767151840"
+        }
+    },
+    {
+        id: 2,
+        title: "Karma",
+        artist: "Summer Walker",
+        genre: "Dark R&B",
+        release_date: new Date("2018"),
+        runtime: "3:08",
+        cover: songPFP2,
+        links: {
+            spotify: "https://open.spotify.com/track/2Fyjjpg03fn7n5cj0Qm380",
+            apple: "https://music.apple.com/ca/song/karma/1438765304",
+            soundcloud: "https://soundcloud.com/summerwalker/karma?in=j1m3n4/sets/tv-ma"
+        }
     }
 ]
     return(
         <>
-        <main>
-            <img className="songPFP"src={songPFP} alt="songpic" />
-            <div className="song-info">
-                <h2>{songs[0].title}</h2>
-                <p>{songs[0].artist}</p>
-                <p>Genre: {songs[0].genre}</p>
-                <p>Release date: {songs[0].release_date.toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                })}</p>
-                <p>Runtime: {songs[0].runtime}</p>
-            </div>
-        </main>
-        <div>
-        <Links/>
-        </div>
+        <section className="song-item">
+            <ul>
+                {songs.map((song)=> (
+                    <li key={song.id}>
+                        <img className="songPFP"src={song.cover} alt="songpic" />
+                        <div className="song-info">
+                            <h2>{song.title}</h2>
+                            <p>{song.artist}</p>
+                            <p>Genre: {song.genre}</p>
+                            <p>Release date: {song.release_date.toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                            })}</p>
+                            <p>Runtime: {songs[0].runtime}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </section>
         </>
     )
 }
--- src/components/pages/DiscoverSongs.tsx
+++ src/components/pages/DiscoverSongs.tsx
@@ -1,11 +1,9 @@
 import SongItem from "../common/song-items/SongItem"
-import SongItem2 from "../common/song-items/SongItem2"
 
 function DiscoverSongs() {
     return(
         <>
         <SongItem />
-        <SongItem2/>
         </>
     )
 }
