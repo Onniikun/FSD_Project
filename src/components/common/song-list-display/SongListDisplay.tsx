733cce7911a6790475bb714fe7179ce475222438
Phoenixx Ordonez <pordonez@academic.rrc.ca>
2026-02-01 09:54:03 -0600

    Created a component to display a list of song lists

---
+++ src/components/common/song-list-display/SongListDisplay.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import type { SongList } from "../../../types/songListTypes";
+import { SongListCard } from "../song-list-card/SongListCard";
+
+export interface SongListsDisplayProps {
+  lists: SongList[];
+}
+
+export default function SongListsDisplay({ lists }: SongListsDisplayProps) {
+    const [expandedId, setExpandedId] = useState<string | null>(null);
+
+    const handleTitleClick = (id: string) => {
+        setExpandedId((prev) => (prev === id ? null : id));
+    };
+
+    return (
+        <div className="lists-display">
+          <h2>All Song Lists</h2>
+
+          {lists.length === 0 && <p>No lists created yet.</p>}
+
+          {lists.map((list) => (
+            <SongListCard
+              key={list.id}
+              list={list}
+              isExpanded={expandedId === list.id}
+              onTitleClick={handleTitleClick}
+            />
+          ))}
+        </div>
+    );
+};
