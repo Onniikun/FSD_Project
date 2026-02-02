40782885f7587772c42351c6d1623e4477490365
Onniikun <dominick2004natoza@gmail.com>
2026-02-01 20:22:11 -0600

    Fixed import issue.

--- src/components/common/songs/song-items/SongItem.tsx
+++ src/components/common/songs/song-items/SongItem.tsx
@@ -4,7 +4,7 @@ import "./SongItem.css"
 import type { Song } from "../../../../types/songModel.ts"
 import GenreFilter from "../song-fliter/genreFilter.tsx"
 import Links from "../song-links/SongLinks.tsx"
-import Upvote from "../song-upvote/Songupvote.tsx"
+import Upvote from "../song-upvote/SongUpVote.tsx"
 import { useState } from "react"
 import { Link } from 'react-router-dom'
 
