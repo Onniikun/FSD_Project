b856362827f792e147f1299589520acfc62ee8d1
Onniikun <dominick2004natoza@gmail.com>
2026-02-16 17:55:22 -0600

    Added Song Item Repo.

--- src/apis/SongItemRepo.ts
+++ src/apis/SongItemRepo.ts
@@ -7,19 +7,47 @@ import { songs } from "../apis/SongItemData.ts"
  */
 export function fetchAllSongs(): Song[] {
     return songs;
-}
+};
 
 /**
  * Retrieves song based on ID number.
  * @param id - Song ID number.
  * @returns Song Item.
  */
-export function fetchSongById(id: number): Song {
-    const getSongId = songs.find(t => t.id === id);
+export function fetchSongById(songId: number): Song {
+    const getSongId = songs.find(t => t.id === songId);
 
     if(!getSongId) {
-        throw new Error(`Cannot find song with id number ${id}`);
+        throw new Error(`Cannot find song with id number ${songId}`);
     }
 
     return getSongId;
+};
+
+/**
+ * Updates an existing song item.
+ * @param updateSongItem - Updated song item data.
+ * @returns - The new updated Song Item.
+ */
+export async function updateSongItem(updateSongItem: Song): Promise<Song> {
+    const songItem = songs.findIndex(t => t.id === updateSongItem.id)
+
+    if(songItem === -1) {
+        throw new Error(`Failed to update song item ${updateSongItem.id}`)
+    }
+    songs[songItem] = updateSongItem;
+    return songs[songItem];
+};
+/**
+ * Deletes song item.
+ * @param songId - Song Item ID.
+ */
+export async function deleteSongItem(songId: number) {
+    const deleteSong = songs.filter(t => t.id === songId)
+
+    if(!deleteSong) {
+        throw new Error(`Failed to delete Song Item ${songId}`)
+    } else 
+
+    return deleteSong;
 }
