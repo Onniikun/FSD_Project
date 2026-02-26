1c78c46dece2d270874b52c87acf3a0cf55d73a8
Onniikun <dominick2004natoza@gmail.com>
2026-02-25 21:54:21 -0600

    Fixed changes based on PR review.

--- src/components/common/songs/song-filter/GenreFilter.tsx
+++ src/components/common/songs/song-filter/GenreFilter.tsx
@@ -6,14 +6,16 @@ type GenreFilterProps = {
 };
 /**
  * Filters songs by Genre.
- * @param param0 - Selected Genre.
+ * @param onChange - Callback when the genre display is changed.
  * @returns - Filtered song.
  */
 function GenreFilter({ onChange }: GenreFilterProps) {
     const genres = ["All", "R&B", "Dark R&B", "K-Pop"];
 
-    //Using my hook
-    const { selectedItem: selectedGenre, setSelectedItem: setSelectedGenre} = useSortFilter("All")
+    const { 
+        selectedItem: selectedGenre, 
+        setSelectedItem: setSelectedGenre
+    } = useSortFilter("All")
 
     const handleGenreChange = (genre: string) => {
         setSelectedGenre(genre)
@@ -21,19 +23,20 @@ function GenreFilter({ onChange }: GenreFilterProps) {
     }
 
     return(
-        <>
         <section className="genre-filter">
             {genres.map((genre) =>(
                 //https://react.dev/learn/rendering-lists
                 <button key={genre} type="button"
                 className={
-                    selectedGenre === genre ? "genre-btn active" : "genre-btn"}
+                    selectedGenre === genre 
+                    ? "genre-btn active" 
+                    : "genre-btn"
+                }
                 onClick={() => handleGenreChange(genre)}>
                 {genre}
             </button>
             ))}
         </section>
-        </>
     )
 }
 export default GenreFilter
--- src/components/common/songs/song-links/SongLinks.tsx
+++ src/components/common/songs/song-links/SongLinks.tsx
@@ -5,50 +5,45 @@ import { useSortFilter } from "../../../hooks/useSortAndFilterUI"
 type LinksProps = {
     links?: songLinks
 }
+
+ // A map list of song platforms.(To add more)
+const platform_map:  Record<string, keyof songLinks> = {
+    Spotify: "spotify",
+    "Apple Music": "apple",
+    "SoundCloud": "soundcloud",
+    Amazon: "amazon",
+    Youtube: "youtube",
+}
 /**
- * Displays URL links of the songs to play.
- * @param param0 - Links(Song URL)
- * @returns - A list of URLs that a song has.
+ * Displays streaming platform links for a song.
+ * @param Links - An object containing platform URLs for the song.
+ * @returns 
  */
 function Links({ links }: LinksProps) {
-    // A map list of song platforms.(To add more)
-    const platform: { [label: string]: keyof songLinks } = {
-        Spotify: "spotify",
-        "Apple Music": "apple",
-        "SoundCloud": "soundcloud",
-        Amazon: "amazon",
-        Youtube: "youtube",
-    }
-    const platforms = Object.keys(platform)
-    //Using my hook
     const { selectedItem, setSelectedItem } = useSortFilter("Spotify")
 
     return(
-    <>
-    <div className="song-media">
-        <ul className="song-links">
-            {platforms.map((label) => {
-                // display url from platform list.
-                const key = platform[label]
-                const url = links?.[key]
-                if (!url)
-                    return null
-                return (
-                    <li key ={label}>
-                        <a href={url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={selectedItem === label ? "active" : ""}
-                        onClick={() => setSelectedItem(label)}
-                        >
-                        {label}
-                        </a>
-                    </li>
-                )
-            })}
+        <div className="song-media">
+            <ul className="song-links">
+                {Object.entries(platform_map).map(([label, key]) => {
+                    // display url from platform list.
+                    const url = links?.[key]
+                    if (!url) return null
+                    return (
+                        <li key ={label}>
+                            <a href={url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={selectedItem === label ? "active" : ""}
+                            onClick={() => setSelectedItem(label)}
+                            >
+                            {label}
+                            </a>
+                        </li>
+                    )
+                })}
         </ul>
     </div>
-    </>
     )
 }
 
