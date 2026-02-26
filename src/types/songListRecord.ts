1e4738ee45a89fbdb8a95df60b61e8a010b3cb1d
Phoenixx Ordonez <pordonez@academic.rrc.ca>
2026-02-26 10:38:02 -0600

    Corrected typo and added 7 more lists

--- src/types/songListRecord.ts
+++ src/types/songListRecord.ts
@@ -4,7 +4,7 @@
  * @param name - The name of the song list
  * @param visibility - The visibility of the song list, either "public" or "private"
  * @param description - A description of the song list
- * @param songIds - An array of song IDs that are included in the song list; ; used for database-friendly storage
+ * @param songIds - An array of song IDs that are included in the song list; used for database-friendly storage
  */
 export interface SongListRecord {
     id: string;
