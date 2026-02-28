0b1eff04182cebd6796c57bb4202ba9a37b53a48
Onniikun <dominick2004natoza@gmail.com>
2026-02-28 02:20:48 -0600

    Removed component to different branch.

---
+++ src/apis/SongItemLinkData.ts
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+export const LinkList = [
+    {value: 'spotify', label: 'spotify'},
+    {value: 'apple', label: 'apple'},
+    {value: 'soundcloud', label: 'soundcloud'},
+    {value: 'amazon', label: 'amazon'},
+    {value: 'youtube', label: 'youtube'},
+]
