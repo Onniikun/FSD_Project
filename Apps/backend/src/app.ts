f4cc62b87f7e9a0fd8a1fa1c5adc1bac566154f0
Onniikun <dominick2004natoza@gmail.com>
2026-04-07 14:39:32 -0500

    removed unread vercel code.

--- Apps/backend/src/app.ts
+++ Apps/backend/src/app.ts
@@ -15,10 +15,6 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  console.log("Incoming request headers:", req.headers);
-  next();
-});
 app.get("/",  (_req, res) => {
     res.send("Got response from backend!");
 });
