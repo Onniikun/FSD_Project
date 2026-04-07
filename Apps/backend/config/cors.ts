a01d307e0d4db3a8a601e4d7c5b80f49c4c20189
Onniikun <dominick2004natoza@gmail.com>
2026-04-07 13:51:10 -0500

    Added more console log comment.

--- Apps/backend/config/cors.ts
+++ Apps/backend/config/cors.ts
@@ -7,10 +7,12 @@ const allowedOrigins = [process.env.FRONTEND_URL];
 console.log("The Front-end URL haha:",allowedOrigins)
 export const corsOptions: CorsOptions = {
   origin(origin, callback) {
+    console.log("The origin haha:",origin)
     // Allow requests from your front-end OR requests with no origin (Postman)
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.log("The origin but blocked haha:",origin)
       callback(new Error("Not allowed by CORS"), false);
     }
   },
