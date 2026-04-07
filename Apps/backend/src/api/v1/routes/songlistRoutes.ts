ec9b9595528dc0af8335b9bafa708785af4f0a45
Onniikun <dominick2004natoza@gmail.com>
2026-04-07 12:21:01 -0500

    Added clerk auth to required routes.

--- Apps/backend/src/api/v1/routes/songlistRoutes.ts
+++ Apps/backend/src/api/v1/routes/songlistRoutes.ts
@@ -2,38 +2,43 @@ import express, { Router } from "express";
 import { validateRequest } from "../middleware/validate";
 import { songlistSchema, songlistUpdateSchema } from "../validations/songlistValidation";
 import * as songlistController from "../controllers/songlistController";
+import { requireAuth } from "@clerk/express";
 
 const router: Router = express.Router();
 
 router.get(
-    "/songlists", 
+    "/songlists",
     songlistController.getAllSonglists
 );
 
 router.get(
-    "/songlists/:id", 
+    "/songlists/:id",
     songlistController.getSonglistById
 );
 
 router.post(
-    "/songlists", 
+    "/songlists",
+    requireAuth(),
     validateRequest(songlistSchema), 
     songlistController.createSonglist
 );
 
 router.put(
-    "/songlists/:id", 
+    "/songlists/:id",
+    requireAuth(), 
     validateRequest(songlistUpdateSchema),
     songlistController.updateSonglist
 );
 
 router.delete(
     "/songlists/:id", 
+    requireAuth(),
     songlistController.deleteSonglist
 );
 
 router.post(
     "/songlists/:id/toggle/:songId",
+    requireAuth(),
     songlistController.toggleSongInList
 );
 
